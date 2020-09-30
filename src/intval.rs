1b6087133ea4b05ef6eb67ffd5f45d2220fc1adc
neetdai <974498346@qq.com>
2020-09-30 22:37:18 +0800

    添加定时器

--- src/intval.rs
+++ src/intval.rs
@@ -1,10 +1,10 @@
 use smol::Timer;
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
+use std::u64::MAX;
 
 #[derive(Debug)]
 pub(super) struct Intval {
     count: u8,
-    timer: Timer,
     init_intval: u64,
 }
 
@@ -12,8 +12,24 @@ impl Intval {
     pub(super) fn new(init_intval: u64) -> Self {
         Self {
             count: 0,
-            timer: Timer::at(Instant::now() + Duration::from_secs(init_intval)),
             init_intval: init_intval,
         }
+    }
+
+    // 推迟执行时间
+    pub(super) fn delay(&mut self) {
+        if self.count < 5 {
+            self.count += 1;
+        }
+    }
+
+    // 重置执行
+    pub(super) fn reset(&mut self) {
+        self.count = 0;
+    }
+
+    pub(super) async fn run(&mut self) -> Instant {
+        Timer::at(Instant::now() + Duration::from_secs(self.init_intval + (self.count * 10) as u64))
+            .await
     }
 }
