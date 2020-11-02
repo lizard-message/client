0b1f8135a927fd72f119330ca453880c7d894a1a
phs <>
2020-11-02 10:25:57 +0800

    暂时修改定时器

--- src/intval.rs
+++ src/intval.rs
@@ -1,11 +1,15 @@
 use smol::Timer;
 use std::time::{Duration, Instant};
-use std::u64::MAX;
+use smol::Task;
+use smol::spawn;
+use std::mem::replace;
 
 #[derive(Debug)]
 pub(super) struct Intval {
     count: u8,
     init_intval: u64,
+    interval_task: Task<Instant>,
+    timeout_task: Task<Instant>,
 }
 
 impl Intval {
@@ -13,6 +17,8 @@ impl Intval {
         Self {
             count: 0,
             init_intval: init_intval,
+            interval_task: spawn(Timer::at(Instant::now())),
+            timeout_task: spawn(Timer::at(Instant::now())),
         }
     }
 
@@ -24,9 +30,14 @@ impl Intval {
     }
 
     // 重置执行
-    pub(super) fn reset(&mut self) {
+    pub(super) async fn reset(&mut self) {
         self.count = 0;
+        
+        let task = replace(&mut self.interval_task, spawn(Timer::at(Instant::now())));
+
+        task.cancel().await;
     }
+
 
     pub(super) async fn run(&mut self) -> Instant {
         Timer::at(Instant::now() + Duration::from_secs(self.init_intval + (self.count * 10) as u64))
