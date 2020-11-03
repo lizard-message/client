e7b547c6a55efe112dd404a4bc3a08a0117f2979
phs <>
2020-11-03 15:20:09 +0800

    添加订阅功能

---
+++ src/subscription.rs
@@ -0,0 +1,29 @@
+use smol::channel::Receiver;
+use smol::spawn;
+use std::marker::Send;
+use std::ops::FnMut;
+
+pub struct Subscription {
+    recv: Receiver<String>,
+}
+
+impl Subscription {
+    pub(super) fn new(recv: Receiver<String>) -> Self {
+        Self { recv }
+    }
+
+    pub async fn with_handle<F>(self, mut proccess: F)
+    where
+        F: FnMut(String) + Send + 'static,
+    {
+        loop {
+            match self.recv.recv().await {
+                Ok(msg) => proccess(msg),
+                Err(e) => {
+                    println!("{:?}", e);
+                    break;
+                },
+            }
+        }
+    }
+}
