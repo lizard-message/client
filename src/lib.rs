e7a9e4010033d1c47e1cc1367ad186f59e65fc74
phs <>
2020-11-03 15:19:41 +0800

    修改select编译时宏展开的递归次数

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#![recursion_limit="256"]
+mod action;
 mod client;
 mod connect_type;
 mod daemon;
 mod error;
 mod intval;
 mod mode;
+mod subscription;
 
 pub use client::{Builder, Client};
 pub use error::Error;
+pub use subscription::Subscription;
