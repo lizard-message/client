fb7c02d65f00d1b12a38b456fe77463415ff0941
phs <>
2020-11-06 16:37:44 +0800

    调整传输的内容

--- src/action.rs
+++ src/action.rs
@@ -1,10 +1,11 @@
+use bytes::BytesMut;
 use smol::channel::Sender;
 
 #[derive(Debug)]
 pub(super) enum Action {
     Sub {
         sub_name: String,
-        msg_sender: Sender<String>,
+        msg_sender: Sender<BytesMut>,
     },
     Pub {
         sub_name: String,
